994ef77d772e977cad7cd4faa889e480f3c93e08
Theo Barrand <tbarrand001@emi.u-bordeaux1.fr>
2018-01-26 16:21:52 +0100

    rebuild

---
+++ Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tbarrand001.bordes_barrand_projet_image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
