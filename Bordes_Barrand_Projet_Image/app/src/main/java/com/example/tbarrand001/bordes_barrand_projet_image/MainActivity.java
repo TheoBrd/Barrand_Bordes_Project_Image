6ec44b8a4506ecde885d4b1683aa03bd615ecad9
Theo Barrand <tbarrand001@emi.u-bordeaux1.fr>
2018-01-26 17:00:54 +0100

    image from gallery

--- Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
+++ Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.tbarrand001.bordes_barrand_projet_image;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private Button buttonLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgView = (ImageView) findViewById(R.id.imageView);
+        buttonLoad = (Button) findViewById(R.id.button);
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, RESULT_LOAD_IMAGE);
+            }
+        });
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            imgView.setImageURI(imageUri);
+
+        }
     }
 }
