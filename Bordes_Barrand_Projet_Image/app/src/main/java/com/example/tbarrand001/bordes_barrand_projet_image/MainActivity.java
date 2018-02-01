717388d70e071b9b0944ed18ec37eec5b1949bc1
Theo Barrand <tbarrand001@emi.u-bordeaux1.fr>
2018-02-01 15:14:07 +0100

    camera button & rotation

--- Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
+++ Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.tbarrand001.bordes_barrand_projet_image;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imgView;
     private static int RESULT_LOAD_IMAGE = 1;
+    private static final int CAMERA_REQUEST = 1888;
     private Button buttonLoad;
+    private Button buttonCam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonCam= (Button) this.findViewById(R.id.button2);
+        buttonCam.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
     }
 
 
@@ -42,7 +56,13 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
             Uri imageUri = data.getData();
             imgView.setImageURI(imageUri);
+        }
 
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(photo);
         }
     }
+
+
 }
