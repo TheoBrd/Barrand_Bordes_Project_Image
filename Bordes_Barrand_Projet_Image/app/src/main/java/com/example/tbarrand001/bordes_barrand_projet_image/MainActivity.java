48fe238b0c8e396ab419ccc70ad894bf799af330
Clara Bordes <cbordes001@emi.u-bordeaux1.fr>
2018-02-16 14:36:34 +0100

    16/02

--- Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
+++ Bordes_Barrand_Projet_Image/app/src/main/java/com/example/tbarrand001/bordes_barrand_projet_image/MainActivity.java
@@ -15,12 +15,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.view.View.OnTouchListener;
+import android.util.Log;
 import android.graphics.PointF;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     private Matrix matrix = new Matrix();
     private Float scale = 1f;
     private ScaleGestureDetector SGD;
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+    private static final String TAG = "Touch";
+    @SuppressWarnings("unused")
+    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
 
 
 
@@ -63,10 +70,129 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        SGD = new ScaleGestureDetector(this, new ScaleLister());
+        imgView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView view = (ImageView) v;
+                view.setScaleType(ImageView.ScaleType.MATRIX);
+                float scale;
+
+                //dumpEvent(event);
+                // Handle touch events here...
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK)
+                {
+                    case MotionEvent.ACTION_DOWN:   // first finger down only
+                        matrix.set(matrix);
+                        start.set(event.getX(), event.getY());
+                        Log.d(TAG, "mode=DRAG"); // write to LogCat
+                        mode = DRAG;
+                        break;
+
+
+                    case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+
+                        mode = NONE;
+                        Log.d(TAG, "mode=NONE");
+                        break;
+
+                    case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+
+                        oldDist = spacing(event);
+                        Log.d(TAG, "oldDist=" + oldDist);
+                        if (oldDist > 5f) {
+                            matrix.set(matrix);
+                            midPoint(mid, event);
+                            mode = ZOOM;
+                            Log.d(TAG, "mode=ZOOM");
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+
+                        if (mode == DRAG)
+                        {
+                            matrix.set(matrix);
+                            matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                        }
+                        else if (mode == ZOOM)
+                        {
+                            // pinch zooming
+                            float newDist = spacing(event);
+                            Log.d(TAG, "newDist=" + newDist);
+                            if (newDist > 5f)
+                            {
+                                matrix.set(matrix);
+                                scale = newDist / oldDist; // setting the scaling of the
+                                // matrix...if scale > 1 means
+                                // zoom in...if scale < 1 means
+                                // zoom out
+                                matrix.postScale(scale, scale, mid.x, mid.y);
+                            }
+                        }
+                        break;
+                }
+
+                view.setImageMatrix(matrix); // display the transformation on screen
+
+                return true; // indicate event was handled
+            }
+
+            private float spacing(MotionEvent event)
+            {
+                float x = event.getX(0) - event.getX(1);
+                float y = event.getY(0) - event.getY(1);
+                return (float) Math.sqrt(x * x + y * y);
+            }
+
+            private void midPoint(PointF point, MotionEvent event)
+            {
+                float x = event.getX(0) + event.getX(1);
+                float y = event.getY(0) + event.getY(1);
+                point.set(x / 2, y / 2);
+            }
+
+
+        });
+
+        //SGD = new ScaleGestureDetector(this, new ScaleLister());
     }
 
-    private class ScaleLister extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+
+
+    /*private void dumpEvent(MotionEvent event)
+    {
+        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
+        StringBuilder sb = new StringBuilder();
+        int action = event.getAction();
+        int actionCode = action & MotionEvent.ACTION_MASK;
+        sb.append("event ACTION_").append(names[actionCode]);
+
+        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
+        {
+            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+            sb.append(")");
+        }
+
+        sb.append("[");
+        for (int i = 0; i < event.getPointerCount(); i++)
+        {
+            sb.append("#").append(i);
+            sb.append("(pid ").append(event.getPointerId(i));
+            sb.append(")=").append((int) event.getX(i));
+            sb.append(",").append((int) event.getY(i));
+            if (i + 1 < event.getPointerCount())
+                sb.append(";");
+        }
+
+        sb.append("]");
+        Log.d("Touch Events ---------", sb.toString());
+    }*/
+
+
+
+    /*private class ScaleLister extends ScaleGestureDetector.SimpleOnScaleGestureListener{
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             scale = scale*detector.getScaleFactor();
@@ -77,7 +203,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+*/
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         SGD.onTouchEvent(event);
